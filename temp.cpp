b71e7012665f1aefebbc46c225f74ae08d651437
Anirudh Pradhan <anipradhan.04@gmail.com>
2024-06-02 20:47:55 +0530

    copilot

--- temp.cpp
+++ temp.cpp
@@ -1,6 +1,17 @@
-#include<iostream>
-using namespace std;
-int main(){
-    int a=9,b=8;
-    cout<<a+b;
+#include <iostream>
+
+int factorial(int n) {
+    if (n == 0 || n == 1) {
+        return 1;
+    } else {
+        return n * factorial(n - 1);
+    }
+}
+
+int main() {
+    int num;
+    std::cout << "Enter a number: ";
+    std::cin >> num;
+    std::cout << "Factorial of " << num << " is: " << factorial(num) << std::endl;
+    return 0;
 }
